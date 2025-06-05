4f89e804a2136d5deaa1e80065c26cd8d71ddae5
Danny6401 <167506458+Danny6401@users.noreply.github.com>
2025-06-05 10:09:18 +0800

    init: first push of component library

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "@fontsource/noto-sans";
+import "@fontsource/noto-sans-tc";
+
+export { Button } from "./component/Atom/Button/Button";
+export type { CustomButtonProps } from "./types/buttonExtends";
+
+export { Fab } from "./component/Atom/Button/FAB";
+export type { CustomFabProps } from "./types/buttonExtends";
+
+export { IconButton } from "./component/Atom/Button/IconButton";
+export type { CustomIconButtonProps } from "./types/buttonExtends";
+
+export { lemonTheme } from "./themes/lemonTheme";
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts"; // 🔥 自動產生 .d.ts
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "@lemon/material",
+      fileName: "index",
+      formats: ["es"], // 支援 ESM 和 CommonJS
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "@mui/material",
+        "@mui/icons-material",
+        "@emotion/react",
+        "@emotion/styled",
+      ], // 不要打包這些
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "@mui/material": "MaterialUI",
+        },
+      },
+    },
+  },
+});
